e97f2c42de0b4cd9c13b2437128111a20d02c706
Yashvardhan Yadav <102205219+yashvy1022@users.noreply.github.com>
2023-07-03 14:14:05 +0530

    Add files via upload

---
+++ Portfolio_Website/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Backend Developer", "Data Science Enthusiast", "ML Enthusiast"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop:true
+});
